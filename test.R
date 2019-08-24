bcf20885907f5ed5630667462f1151c019ab384d
benmbrew <benmbrew@gmail.com>
2019-08-24 15:57:36 -0400

    working on test.R to replicate simulations and AIC scores

--- test.R
+++ test.R
@@ -5,6 +5,18 @@ af <- raw_data_af
 malay <- raw_data_malay
 sen <- raw_data_sen
 
+
+
+# create a function that takes a given country and runs all distribution fits and simulations to compare to excel tools.
+test_dis <- function(data, dis) {
+  
+}
+
+af_sim <- rgamma(n = 15000, shape = af_mle[1], scale = af_mle[2])
+af_output <-  quantile(af_sim,c(0.5,0.8,0.9, 0.96,0.98,0.99)) 
+af_output
+
+
 # mle and aic for each 
 af_aic <- fitdistrplus::fitdist(af$Loss, "gamma", start=list(shape=0.5, scale=1), method="mle")$aic
 af_mle <- fitdistrplus::fitdist(af$Loss, "gamma", start=list(shape=0.5, scale=1), method="mle")$estimate
@@ -15,9 +27,3 @@ malay_mle <- fitdistrplus::fitdist(malay$Loss, "gamma", start=list(shape=0.5, sc
 
 sen_aic <- fitdistrplus::fitdist(sen$Loss, "gamma", start=list(shape=0.5, scale=1), method="mle")$aic
 sen_mle <- fitdistrplus::fitdist(sen$Loss, "gamma", start=list(shape=0.5, scale=1), method="mle")$estimate
-
-# now get output for each data set
-af_sim <- rgamma(n = 15000, shape = af_mle[1], scale = af_mle[2])
-af_output <-  quantile(af_sim,c(0.5,0.8,0.9, 0.96,0.98,0.99)) 
-af_output
-
